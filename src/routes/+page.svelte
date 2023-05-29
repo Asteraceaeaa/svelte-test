aea456cdf460d9cc6265ec485397f9242bc19416
Vladimir <kovinv454@gmail.com>
2023-05-29 16:10:38 +0300

    kr1cha

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1><a href="https://www.youtube.com/@kr1cha">sub to kr1chabtw</a></h1>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->

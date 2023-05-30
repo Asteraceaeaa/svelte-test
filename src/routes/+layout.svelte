87c547cac105fa9c2612e542b6ad7d869a391e74
Vladimir <kovinv454@gmail.com>
2023-05-30 14:31:12 +0300

    nav red

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,42 @@
 <nav>
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Ковин Владимир</a>
-        <div class="nav-links"></div>
-        <a href="/about" class="link">Обо мне</a>
-        <a href="/blog" class="link">Блог</a>
-        <a href="/projects" class="link">Портфолио</a>
-        <a href="/contact" class="link">Контакты</a>
+        <div class="nav-links">
+            {#each nav as link}
+            <a href={link.href} class="link">{link.title}</a>
+            {/each}
+        </div>
     </div>
 </nav>
 
 <div class="container">
     <slot></slot>
 </div>
+<script>
+    const nav =[
+    {
+        title: "Обо мне",
+        href: "/about"
+    },
+    {
+        title: "Блог",
+        href: "/blog"
+    },
+    {
+        title: "Портфолио",
+        href: "/project"
+    },
+    {
+        title: "Контакты",
+        href: "/contact"
+    },
+    {
+        title: "temp",
+        href: "/temp"
+    }
 
+    ] 
+</script>
 <style>
     .container {
         max-width: 1400px;
